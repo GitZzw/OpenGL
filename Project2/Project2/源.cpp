33a8d584057f9c453d8405ffe270b09797467f87
GitZzw <120137016@qq.com>
2020-12-15 16:34:29 +0800

    final distribution

--- Project2/Project2/源.cpp
+++ Project2/Project2/源.cpp
@@ -4,6 +4,7 @@
 直到速度绝对值接近初速度的1/100则中止。请在三维环境中实现该过程，并显示球心的轨迹曲线
 */
 #include <GL/glut.h>
+#include <queue>
 #include <cmath>
 #include <stdlib.h>
 #include <GL/glut.h>
@@ -18,28 +19,39 @@ using namespace std;
 GLfloat WHITE[] = { 1, 1, 1 };    //白色
 GLfloat RED[] = { 1, 0, 0 };    //红色
 GLfloat GREEN[] = { 0, 1, 0 };    //绿色
+GLfloat BLACK[] = { 0, 0, 0 };//Black
 
 								  //鼠标点击界面前后的时间
 clock_t time1;
 clock_t time2;
+clock_t time3;
+clock_t time4;
+
+double User_radius;
 
 //鼠标刚好按下时在界面上的坐标
 double mouseX1;
 double mouseY1;
+double mouseX12;
+double mouseY12;
 
 //鼠标松开时在界面上的坐标
 double mouseX2;
 double mouseY2;
+double mouseX22;
+double mouseY22;
 
 //鼠标移动的速度
-double mouseVX;
-double mouseVY;
+double mouseVX=0;
+double mouseVY=0;
+double mouseVX2=0;
+double mouseVY2=0;
 
 //判断用户是否点击了屏幕
 int flag = 0;
 
 //小球中心点位置（全局变量）
-vector<vector<double>> centerList;
+queue<vector<double>> centerList;
 
 //摄像机类：水平移动半径为10，按上下键则垂直移动
 class Camera {
@@ -50,11 +62,11 @@ public:
 	double dy;         //上下y增量
 public:
 	//类构造函数—默认初始化用法
-	Camera() : theta(0), y(4), dTheta(0.04), dy(0.2) {}
+	Camera() : theta(0), y(10), dTheta(0.04), dy(0.2) {}
 	//类方法
-	double getX() { return 30 * cos(theta); }
+	double getX() { return 0+25 * cos(theta); }
 	double getY() { return y; }
-	double getZ() { return 30 * sin(theta); }
+	double getZ() { return 0+25 * sin(theta); }
 	void moveRight() { theta += dTheta; }
 	void moveLeft() { theta -= dTheta; }
 	void moveUp() { y += dy; }
@@ -68,13 +80,12 @@ public:
 //每帧上下移动0.05单位
 
 //相机初始位置
-float gx = 0;
+float ballx = 0;
 float gy = 3;
-float gz = 0;
+float ballz = 0;
 
 class Ball {
 	//类的属性
-	double radius;
 	GLfloat* color;
 	double maximumHeight;
 	double x;
@@ -82,12 +93,15 @@ class Ball {
 	double z;
 	double vx;
 	double vy;
+	double vx2;
+	double vy2;
 	int direction;   //方向
 public:
+	double radius;
 	//构造函数
 	Ball(double r, GLfloat* c, double h, double x, double z) :
-		radius(r), color(c), maximumHeight(h), direction(-1),
-		y(h), x(x), z(z), vx(0.3), vy(0) {
+		radius(0.5), color(c), maximumHeight(h), direction(-1),
+		y(h), x(x), z(z), vx(0.3), vy(0), vx2(0),vy2(0) {
 	}
 
 	double getX()
@@ -130,6 +144,16 @@ public:
 		vy = mvy;
 	}
 
+	void updateVX2(double mvx)
+	{
+		vx2 = mvx;
+	}
+
+	void updateVY2(double mvy)
+	{
+		vy2 = mvy;
+	}
+
 	void updateDirection(int direct)
 	{
 		direction = direct;
@@ -139,27 +163,65 @@ public:
 	void update() {
 		//正反运动
 		x += vx;
-		vy += direction * 0.0098;
-		y += vy;
-		gx = x;
-		gy = y;
-		gz = z;
-		if (vy > 0 && vy < 0.005) {
-
-			direction = -1;
+		if (flag == 1) {
+			vy2 = 0;
 		}
-		else if (y < radius) {
+		else if (flag == 2) {
+			vy = 0;
+		}
+		vy += direction * 0.0098;
+		vy2 += direction * 0.0098;
+		y += abs(vy) > abs(vy2) ? vy : vy2;
+		z += vx2;
+		ballx = x;
+		gy = y;
+		ballz = z;
+
+		if (y > 10) {
+			y = 10;
+			vy = -vy;
+			vy2 = -vy2;
+			vy = vy * 0.80;
+			vy2 = vy2 * 0.80;
+			vx = vx * 0.85;
+			vx2 = vx2 * 0.85;
+		
+		}
+
+
+		if (y < radius) {
 			y = radius;
 			vy = -vy;
+			vy2 = -vy2;
+
 			vy = vy * 0.80;
+			vy2 = vy2 * 0.80;
 			vx = vx * 0.85;
+			vx2 = vx2 * 0.85;
 		}
 
-		if (sqrt(vx * vx + vy * vy) < sqrt(mouseVX * mouseVX + mouseVY * mouseVY) / 100)
+
+		if (sqrt(vx * vx + vy * vy + vx2 * vx2) < sqrt(mouseVX * mouseVX + mouseVY * mouseVY + mouseVX2 * mouseVX2) / 100)
 		{
 			vx = 0;
 			vy = 0;
+			vx2 = 0;
+			vy2 = 0;
 		}
+
+
+		if (x < 0 || x>47) {
+			if (x<0)x = 0;
+			else x = 47;
+			vx = -vx*0.85;
+		}
+		if (z< 0 || z>47) {
+			if(z<0)z = 0;
+			else z = 47;
+			vx2 = -vx2*0.85;
+		}
+
+
 
 		glPushMatrix();
 		//单独设置每个球的材质参数
@@ -173,7 +235,7 @@ public:
 };
 
 //棋盘格：沿x和z平面分布
-//点光源位置设置为(4, 3, 7).
+//点光源位置设置为(10，25，25).
 class Checkerboard {
 	int displayListId;
 	int width;
@@ -194,8 +256,8 @@ public:
 										   //新建操作表
 		glNewList(displayListId, GL_COMPILE);   //把下述命令装入显示列表但不显示
 
-												//光源位置参数
-		GLfloat lightPosition[] = { 4, 3, 7, 1 };
+												//光源位置参数棋盘中心xz50*50
+		GLfloat lightPosition[] = { 10, 25, 25, 1 };
 
 		//设置光源位置
 		glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
@@ -227,18 +289,19 @@ public:
 };
 
 //全局变量：棋盘格、相机和3个球的数组
-Checkerboard checkerboard(100, 100);
+Checkerboard checkerboard(50, 50);
 Camera camera;
 //创建3个小球的数组
 
-//小球的初始坐标
-double initBallH = 8;
-double initBallX = 5;
-double initBallZ = 15;
+//小球的初始坐标x50 z50
+//棋盘大小为x*z=100*100
+//x方向平行屏幕向左 z垂直屏幕向里
+double initBallH = 5;
+double initBallX = 25;
+double initBallZ = 25;
 
 Ball balls[] = {
-
-	Ball(0.5, WHITE, initBallH, initBallX, initBallZ)
+	Ball(User_radius, GREEN, initBallH, initBallX, initBallZ)
 };
 
 
@@ -259,6 +322,8 @@ void init() {
 	glEnable(GL_LIGHT0);
 	//创建棋盘格
 	checkerboard.create();
+
+	
 }
 
 //自定义绘制函数,通过类绘制各对象，display函数代码得以简化
@@ -271,7 +336,7 @@ void display() {
 
 	//设置视角——摄像机参数
 	gluLookAt(camera.getX(), camera.getY(), camera.getZ(),    //摄像机位置
-		gx, 3, gz,   //焦点坐标
+		ballx, 3, ballz,   //焦点坐标
 		0.0, 1.0, 0.0);   //摄像机机顶方向矢量
 
 						  //绘制棋盘
@@ -279,6 +344,7 @@ void display() {
 
 	//绘制小球
 	for (int i = 0; i < sizeof balls / sizeof(Ball); i++) {
+
 		if (flag == 0)
 		{
 			//如果没有鼠标的动作，则让小球一直保持在起点的位置
@@ -288,7 +354,7 @@ void display() {
 			balls[i].updateDirection(0);
 			balls[i].update();
 		}
-		else if (flag == 1)
+		else if (flag == 1 || flag == 2)
 		{
 			//更新位置并绘图
 			balls[i].updateDirection(-1);
@@ -304,18 +370,23 @@ void display() {
 			cent.push_back(balls[i].getX());
 			cent.push_back(balls[i].getY());
 			cent.push_back(balls[i].getZ());
-
 			//添加到全局变量centerList中
-			centerList.push_back(cent);
+			if (centerList.size() == 200) {
+				centerList.pop();
+			}
+			centerList.push(cent);
 
 			//绘制小球运动的轨迹
 			glColor3f(1.0f, 0.0f, 0.0f);
 			glPointSize(5.0f);
 			glBegin(GL_POINTS);
+			queue<vector<double>> tempQueue(centerList);
 			for (int i = 0; i < centerList.size(); i++)
 			{
 				//绘制圆心点
-				glVertex3f(centerList[i][0], centerList[i][1], centerList[i][2]);
+				vector<double> temp = tempQueue.front();
+				tempQueue.pop();
+				glVertex3f(temp[0], temp[1], temp[2]);
 			}
 			glEnd();
 		}
@@ -388,6 +459,39 @@ void mouseCB(int button, int state, int x, int y)
 		balls[0].updateVY(mouseVY);
 	}
 
+	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	{
+		time3 = clock(); //获取鼠标按下时的时间（毫秒）
+		cout << "time2:" << time3 << endl;
+		mouseX12 = x;
+		mouseY12 = y;
+		cout << "1 Mouse x:" << x << endl;
+		cout << "1 Mouse y:" << y << endl;
+	}
+	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP)
+	{
+		time4 = clock(); //获取鼠标松开时的时间（毫秒）
+		cout << "time2:" << time4 << endl;
+		mouseX22 = x;
+		mouseY22 = y;
+		cout << "2 Mouse x:" << x << endl;
+		cout << "2 Mouse y:" << y << endl;
+
+		//计算鼠标在x和y方向的拖动速度
+		mouseVX2 = (mouseX12 - mouseX22) / (time4 * 1.0 - time3 * 1.0);
+		mouseVY2 = (mouseY12 - mouseY22) / (time4 * 1.0 - time3 * 1.0);
+
+		mouseVX2 = mouseVX2 / 2;
+		mouseVY2 = mouseVY2 / 2;
+
+		cout << "mouseVX2 = " << mouseVX2 << endl;
+		cout << "mouseVY2 = " << mouseVY2 << endl;
+
+		flag = 2;
+		balls[0].updateVX2(mouseVX2);
+		balls[0].updateVY2(mouseVY2);
+	}
+
 	glutPostRedisplay();
 }
 
@@ -401,6 +505,12 @@ void mouseMove(int x, int y)
 }
 
 int main(int argc, char** argv) {
+	cout << "请输入球的半径大小，单位是m" << endl;
+	cin >> User_radius;
+	for (auto i=0; i < sizeof balls / sizeof(Ball); ++i) {
+		balls[i].radius = User_radius;
+	}
+	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowPosition(80, 80);
